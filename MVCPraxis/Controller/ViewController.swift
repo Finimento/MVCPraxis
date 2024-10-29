216e7e7b8c0e202107e144cfef8afbf96b682023
fiesematente <mosesfinimento@googlemail.com>
2024-10-29 17:44:25 +0100

    stage all files (i forgot about it in the commit before).

--- MVCPraxis/Controller/ViewController.swift
+++ MVCPraxis/Controller/ViewController.swift
@@ -12,10 +12,16 @@ class ViewController: UIViewController {
     //MARK: - Outlet
     
     @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: - Data
+    var persons = Person.loadPersons()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tableView.tableFooterView = UIView()
     }
 
 
@@ -23,16 +29,20 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return persons.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = persons[indexPath.row].vorname + " " + persons[indexPath.row].nachname
+        return cell
     }
     
 }
 
 extension ViewController: UITableViewDelegate{
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
--- MVCPraxis/Model/Person.swift
+++ MVCPraxis/Model/Person.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class Person {
+    
+    var nachname: String
+    var vorname: String
+    
+    init(nachname: String, vorname: String) {
+        self.nachname = nachname
+        self.vorname = vorname
+    }
+    
+    static func loadPersons() -> [Person]{
+        var personsArray = [Person]()
+        
+        personsArray.append(Person(nachname: "Friedrichson", vorname: "Franz"))
+        personsArray.append(Person(nachname: "Giesinger", vorname: "Josef"))
+        personsArray.append(Person(nachname: "Kira", vorname: "Bundspecht"))
+        return personsArray
+    }
+    
+}
